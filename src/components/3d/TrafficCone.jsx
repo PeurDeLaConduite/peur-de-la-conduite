ea664ac71724f4d3c4e30a2d14c9faff161b3ef8
Jerem16 <jeremy.lemaignent@gmail.com>
2025-04-06 15:06:37 +0200

    init 23

--- src/components/3d/TrafficCone.jsx
+++ src/components/3d/TrafficCone.jsx
@@ -1,26 +1,26 @@
-"use client";
+// "use client";
 
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, useGLTF } from "@react-three/drei";
 
-function TrafficCone({ title }) {
-    const { scene } = useGLTF("/models/cone.glb"); // Charge le modèle
-    return <primitive object={scene} />;
-}
+// function TrafficCone({ title }) {
+//     const { scene } = useGLTF("/models/cone.glb"); // Charge le modèle
+//     return <primitive object={scene} />;
+// }
 
-export default function Scene({ title }) {
-    return (
-        <>
-            <div className="f">
-                <h1>{title}</h1>
-                <p>Cette page est en construction</p>
-            </div>
-            <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[2, 2, 2]} />
-                <TrafficCone />
-                <OrbitControls />
-            </Canvas>
-        </>
-    );
-}
+// export default function Scene({ title }) {
+//     return (
+//         <>
+//             <div className="f">
+//                 <h1>{title}</h1>
+//                 <p>Cette page est en construction</p>
+//             </div>
+//             <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+//                 <ambientLight intensity={0.5} />
+//                 <directionalLight position={[2, 2, 2]} />
+//                 <TrafficCone />
+//                 <OrbitControls />
+//             </Canvas>
+//         </>
+//     );
+// }
