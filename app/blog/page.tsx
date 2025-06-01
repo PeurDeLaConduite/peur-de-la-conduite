f0630c447151c4bdc92dc2e0846b742739d9a2aa
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-01 16:20:38 +0200

    debug blog missing article 2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,18 +3,16 @@ import { Metadata } from "next";
 import SectionContainer from "./SectionContainer";
 import BlogClientWrapper from "./BlogClientWrapper";
 import BlogIcon from "@components/svg_Icon/Blog";
-import { loadData } from "@utils/blogData/loadData";
+
 
 export const metadata: Metadata = {
     title: "Blog",
 };
 
 export default async function Page() {
-    const data = await loadData(); // FETCH côté serveur !
-
     return (
         <SectionContainer id="blog" title="Blog" icon={<BlogIcon />}>
-            <BlogClientWrapper initialData={data} />
+            <BlogClientWrapper />
         </SectionContainer>
     );
 }
