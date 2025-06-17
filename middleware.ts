663ad0f8b21d53eb546850a547577692c2079aa6
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-17 17:10:09 +0200

    feature Base One

--- middleware.ts
+++ middleware.ts
@@ -8,18 +8,34 @@ export function middleware(request: NextRequest) {
     const isTablet =
         /(iPad|Tablet)/i.test(ua) ||
         (/(Android)/i.test(ua) && !/Mobile/i.test(ua));
+
     const isMobile =
         !isTablet &&
         /Mobi|Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
 
-    // Si mobile → redirige vers le sous-domaine mobile
     if (isMobile) {
         const destination = `https://mobile.peur-de-la-conduite.fr${request.nextUrl.pathname}${request.nextUrl.search}`;
-        return NextResponse.redirect(destination);
+        const response = NextResponse.redirect(destination);
+        response.cookies.set("deviceType", "mobile", {
+            path: "/",
+            domain: ".peur-de-la-conduite.fr",
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+        });
+        return response;
     }
 
-    // Sinon (desktop, tablette, bots…) → ne fais rien
-    return NextResponse.next();
+    const response = NextResponse.next();
+    response.cookies.set("deviceType", "desktop", {
+        path: "/",
+        domain: ".peur-de-la-conduite.fr",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+    });
+
+    return response;
 }
 
 export const config = {
