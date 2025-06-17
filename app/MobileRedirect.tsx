b670773704ca10cc2598107d6bf1c31ef561fbf3
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-17 18:15:13 +0200

    feature Test MobileRedirec Cookies

--- app/MobileRedirect.tsx
+++ app/MobileRedirect.tsx
@@ -4,8 +4,16 @@ import { useEffect } from "react";
 export default function MobileRedirect() {
     useEffect(() => {
         if (window.innerWidth < 900) {
-            window.location.href = "https://peur-de-la-conduite.fr" + window.location.pathname + window.location.search;
+            // Définition du cookie
+            document.cookie = `deviceType=mobile; path=/; domain=.peur-de-la-conduite.fr; Secure; SameSite=Strict`;
+
+            // Redirection vers la version mobile
+            window.location.href =
+                "https://peur-de-la-conduite.fr" +
+                window.location.pathname +
+                window.location.search;
         }
     }, []);
+
     return null;
 }
