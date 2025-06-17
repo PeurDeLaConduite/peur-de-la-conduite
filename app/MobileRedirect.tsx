f42a61a6f3af0812cf1c46c9ef9377c6d7137b1a
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-17 17:44:53 +0200

    feature Test MobileRedirec mobile.https://peur-de-la-conduite.fr

--- app/MobileRedirect.tsx
+++ app/MobileRedirect.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function MobileRedirect() {
     useEffect(() => {
         if (window.innerWidth < 900) {
-            window.location.href = "https://mobile.peur-de-la-conduite.fr" + window.location.pathname + window.location.search;
+            window.location.href = "https://peur-de-la-conduite.fr" + window.location.pathname + window.location.search;
         }
     }, []);
     return null;
