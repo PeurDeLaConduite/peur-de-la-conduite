e1cfb0d1863efb5025202255a540deac8cf5e0ef
Jerem16 <jeremy.lemaignent@gmail.com>
2025-10-23 17:44:20 +0200

    start

--- app/MobileRedirect.tsx
+++ app/MobileRedirect.tsx
@@ -4,7 +4,7 @@ import { useEffect, useCallback } from "react";
 export default function MobileRedirect() {
     const handleRedirect = useCallback(() => {
         if (window.innerWidth < 900) {
-            const target = `https://mobile.peur-de-la-conduite.fr${window.location.pathname}${window.location.search}`;
+            const target = `https://peur-de-la-conduite.fr${window.location.pathname}${window.location.search}`;
             window.location.href = target;
         }
     }, []);
